669eb36f4be8784b632ef8eabac4d665eccbe2db
Vivian-Oliveira <costa.vivs@gmail.com>
2021-06-19 13:10:07 +0100

    desafio

--- iterisImoveis/src/app/components/home-page/home-page.component.ts
+++ iterisImoveis/src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ImoveisApiModel } from 'src/app/services/imoveis-api-model.';
+import { ImoveisApiService } from 'src/app/services/imoveis-api.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  listaImagens: Array<string> = []
 
-  constructor() { }
+  imagemAtual: string = ''
+
+  constructor(public imoveisApi: ImoveisApiService) { }
 
   ngOnInit(): void {
+    this.imoveisApi.get().subscribe({
+      next: (retornoDaApi) => {
+        this.imagemAtual = retornoDaApi[0].image
+        this.listaImagens = retornoDaApi.map((item: ImoveisApiModel) => {
+          return item.image
+        })
+      }
+    })
+  }
+
+  prev(): void {
+    const index = this.listaImagens.indexOf(this.imagemAtual)
+    if(index == 0) return
+    this.imagemAtual = this.listaImagens[index -1]
+
+  }
+
+  next(): void {
+    const index = this.listaImagens.indexOf(this.imagemAtual)
+
+    if(index == this.listaImagens.length - 1) return
+    this.imagemAtual = this.listaImagens[index - 1]
   }
 
 }
